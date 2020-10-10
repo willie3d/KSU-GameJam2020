8cbe06a04d1084c6110b86c65bf09a3767ccb821
willie3d <williedoyle@fernbanklinks.com>
2020-10-10 03:04:52 -0400

    Working on Rollong text

---
+++ GameJam2020/Assets/TextWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+
+{
+    private Text uiText;
+    private string textToWrite;
+    private int characterIndex;
+    private float timePerCharacter;
+    private float timer;
+
+    public void AddWriter(Text uiText, string textToWrite, float timePerCharacter)
+    {
+        this.uiText = uiText;
+        this.textToWrite = textToWrite;
+        this.timePerCharacter = timePerCharacter;
+        characterIndex = 0;
+    }
+    private void Update()
+    {
+        if (uiText != null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                timer += timePerCharacter;
+                characterIndex++;
+                uiText.text = textToWrite.Substring(0, characterIndex);
+            }
+        }
+    }
+}
---
+++ GameJam2020/Assets/UI_Assistant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Assistant : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+    private void Awake()
+    {
+        messageText = transform.Find("Message").Find("messageText").GetComponent<Text>();
+    }
+    private void Start()
+    {
+        messageText.text = "hello citizens";
+    }
+}
